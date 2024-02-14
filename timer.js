ceade1be4e4b78a6c4b12e97d0d2b1e04594a722
Neil Chadwick <neilchadders@hotmail.com>
2024-02-14 19:49:59 +0000

    add getExercise method to obj

--- timer.js
+++ timer.js
@@ -1,47 +1,52 @@
 
-//Initialising variables
-/*
+//Initialising var
 const initial = document.getElementById("initial");
 let begin = 2;
-let timeleft = document.getElementById("time")
-const timer = c
+let timer = document.getElementById("timer")
 const exercise = document.getElementById("exercise");
 const exArr = ["Push Up", "Kettlebell Swing", "Kettlebell Press", "Rows", "Plank", "Burpees", "Goblet Squat"];
 const click = document.getElementById("start").addEventListener("click", callAllCounts, { once: true });
 const clear = document.getElementById("clear").addEventListener("click", startAgain);
 let pauseBool = true;
-*/
 
-let timeleft = document.getElementById("time")
+let timeleft =  document.getElementById("time")
+
 class Workout{
     constructor( timeleft){
-      this.timeleft = timeleft
+      this.timeleft = timeleft.value
     }
-    showTime(){
-      console.log(this.timeleft.value)
+    setTime(){
+      timer.innerHTML = timeleft.value // trigerred in HTML on submit button
+    }
+    setExercise(){
+      setInterval(function () {
+        if(timeleft.value >= 0 && pauseBool)
+        timer.innerHTML = timeleft.value --
+        //console.log(timer.innerHTML)
+        if(timer.innerHTML % 10 === 0 && timer.innerHTML > 0 && pauseBool) {
+        let x = Math.floor(Math.random() * 7);
+        exercise.innerHTML = exArr[x]
+          }
+      }, 1000);
     }
 }
 
 const workout1 = new Workout(timeleft) 
-workout1.showTime
 
 
-function callAllCounts() {
+
+const callAllCounts = () => {
   initialCount()
   setTimeout(mainCount, 3000)
-  //setTimeout(generateExercise, 3000)
-
+  setTimeout(workout1.setExercise, 3000)
 }
 
-function pause() {
-  pauseBool = !pauseBool;
+const pause = () => pauseBool = !pauseBool;
 }
 
 
 // Initial 3,2,1 countdown function
 function initialCount() {
-
-
   initial.classList.remove("hide"); //Removes hidden css property 
   exercise.classList.remove("hide");
 
@@ -62,51 +67,15 @@ function initialCount() {
 
 // Main countdown function 
 function mainCount() {
-
-  const i = Math.floor(Math.random() * 7);
+  let i = Math.floor(Math.random() * 7);
   exercise.innerHTML = exArr[i]; // Generates first random exercise from the array
 
   setInterval(function () {
-
-
-    if (timeleft <= 998) {
-      initial.classList.add("initial"); //Hides the initial countdown after 1 second
-    }
-
-    if (timeleft >= 0 && pauseBool === true) { // Gives the pause button functionality
-      timer.innerHTML = timeleft; // The thousand second countdown
-      timeleft -= 1;
-    }
-
-    if (timeleft % 10 === 9 && pauseBool === true) {
-      const i = Math.floor(Math.random() * 7);
-      exercise.innerHTML = exArr[i]
-      console.log(exercise)
-    }
-
+      initial.classList.add("initial"); //Hides the initial countdown after 1 second   
   }, 1000); //1 second decrements
 }
 
 
-/*function generateExercise() { //randomly picks a new exercise every 10 seconds
-
-    
-
-}
-  setInterval(function () {
-
-
-    const i = Math.floor(Math.random() * 7);
-    exercise.innerHTML = exArr[i];
-
-    if (timeleft >= 0) {
-      exercise.innerHTML = exArr[i];
-    }
-
-  }, 10000); //10 seconds decrements for each excercise
-}
-
-*/
 
 function startAgain() { // Reloads the entire program
   return window.location.reload();
