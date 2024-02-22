d77509722e84f9e6a222d7434a3872069645e8e1
Neil Chadwick <neilchadders@hotmail.com>
2024-02-22 06:58:45 +0000

    finalise time buttons

--- timer.js
+++ timer.js
@@ -6,7 +6,7 @@ const initial = document.getElementById("initial");
 let begin = 2;
 
 
-let timer = document.getElementById("timer")
+const timer = document.getElementById("timer")
 const exercise = document.getElementById("exercise");
 let exArr = []
 
@@ -15,26 +15,30 @@ let exArr = []
 const click = document.getElementById("start").addEventListener("click", callAllCounts, { once: true });
 const clear = document.getElementById("clear").addEventListener("click", startAgain);
 let pauseBool = true;
+let buttonBool = false;
 
-let timeleft =  document.getElementById("time")
+//const timeleft =  document.getElementById("time")
 let minutes = document.getElementById("minutes")
 let seconds = document.getElementById("seconds")
+let buttonClick = 0;
+let secs;
 
 //Constructor
 class Workout{
 
-      constructor( minutes){
+      constructor( minutes, seconds){
+     
       this.minutes = minutes.value
       this.seconds = seconds.value
      
     }
 
-    burpee(assignId){
-      let ex = assignId
+    setExercise(assignId){
       
+      let ex = assignId
       if(!exArr.includes(ex)) {
         exArr.push(document.getElementById(ex).value)
-       } else{
+      } else{
       let x = exArr.indexOf(ex)
       exArr.splice(x,1)
     } if(ex === "Select All") {
@@ -42,42 +46,22 @@ class Workout{
     }
     console.log(exArr)
     }
-
-    /*
-     burpee = () => workout1.addToArr("Burpee") 
-     plank = () => workout1.addToArr("Plank")
-     rest = () => workout1.addToArr("Rest")
-     squat = () => workout1.addToArr("Goblet Squat")
-      /*
-        if(!exArr.includes("Burpee")) {
-          exArr.push(document.getElementById("Burpee").value)
-         } else{
-        let x = exArr.indexOf("Burpee")
-        exArr.splice(x,1)
-      } */
-      
+ 
     setTime(){ 
-     
+      
       let formatZero = ( (minutes.value < 10) ? "0" : "" ) + minutes.value + ":" + ( (seconds.value < 10) ? "0" : "" ) + seconds.value;
       timer.innerHTML = formatZero
-      //timer.innerHTML = `${minutes.value}:${seconds.value}` // trigerred in HTML on submit button
-    }
-    setExercise(){      
-      setInterval(function () {
-        if(timeleft.value >= 0 && pauseBool)
-        timer.innerHTML = timeleft.value -- //This subtracts 1 every second
-       
-        if(timer.innerHTML % 10 === 0 && timer.innerHTML > 0 && pauseBool) { //selects random exercise
-    
-        exercise.innerHTML = exArr[Math.floor(Math.random() * exArr.length)]
-          console.log(exArr)
-          }
-      }, 1000);
+     // trigerred in HTML on submit button
     }
 
+    timeButton = (id) => {
+      minutes.value= parseInt(id)
+      seconds.value = 0
+    
+    }
 }
 
-const workout1 = new Workout(minutes) 
+const workout1 = new Workout(minutes, seconds) 
 
 
 
@@ -88,7 +72,11 @@ function callAllCounts() {
   setTimeout(setExercise, 3000)
 }
 
-const pause = ()=> pauseBool = !pauseBool;
+let pause = ()=> {
+  pauseBool = !pauseBool;
+  console.log(exArr)
+}
+
 
 // Initial 3,2,1 countdown function
 const initialCount = () => {
@@ -109,11 +97,13 @@ const initialCount = () => {
 }
 
 let timeInSecs;
-let ticker;
+
 
 function setExercise() {
-let secs = (minutes.value * 6 ) + (seconds.value)
-console.log(minutes.value, seconds.value)
+let test = parseInt(seconds.innerHTML)
+
+secs = parseInt(minutes.value) * 60  + parseInt(seconds.value)
+if (pauseBool)
 timeInSecs = secs;
 ticker = setInterval("tick()", 1000); 
 }
@@ -122,25 +112,26 @@ function tick( ) {
 if (timeInSecs > 0 && pauseBool) {
 timeInSecs--; 
 }
-else {
-clearInterval(ticker);
- 
-}
+
 
 console.log(timeInSecs)
 
 let mins = Math.floor(timeInSecs/60);
 secs = timeInSecs %60;
 let formatZero = ( (mins < 10) ? "0" : "" ) + mins + ":" + ( (secs < 10) ? "0" : "" ) + secs;
-console.log(exArr.length)
-if( secs % 10 === 0) exercise.innerHTML = exArr[Math.floor(Math.random() * exArr.length-1)]
+
+let z = exArr[Math.floor(Math.random() * exArr.length-1)]
+
+if( secs % 10 === 0) exercise.innerHTML = z
+
+
 timer.innerHTML = formatZero;
 }
 
 
 // Main countdown function 
 function mainCount() {
- let i = Math.floor(Math.random() * 7);
+ let i = Math.floor(Math.random() * exArr.length-1);
   exercise.innerHTML = exArr[i]; // Generates first random exercise from the array
 
   setInterval(function () {
@@ -154,36 +145,3 @@ function startAgain() { // Reloads the entire program
   return window.location.reload();
 } 
 
-
-
-
-// ADD EXERCISE
-
-
-
-/*
-
-const createNote = (uid, title, text, date) => {
-  const note = document.createElement("div");
-  note.className = "note";
-  note.id = uid;
-  note.innerHTML = `
-    <div class="note-title">${title}</div>
-    <div class="note-controls">
-      <button class="note-edit" onclick="editNote(${uid})">
-        Edit
-      </button>
-      <button class="note-save" style="display:none" onclick="saveNote(${uid})">
-        Save
-      </button>
-      <button class="note-delete" onclick="deleteNote(${uid})">
-        Delete
-      </button>
-    </div>
-    <div class="note-text">${text}</div>
-    <div class="note-date">${date}</div>
-  `;
-
-  notesWrapper.insertBefore(note, notesWrapper.firstChild);
-};
-*/
