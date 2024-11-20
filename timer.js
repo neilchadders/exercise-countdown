2331a81efcb74ba6e026dac92c124c871f2048ed
Neil Chadwick <neilchadders@hotmail.com>
2024-11-20 09:41:15 +0000

    amend visibility of time buttons

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 
-//ADD Exercises - 
+//Variables
 
 const addExerciseButtons = document.getElementsByClassName("add-btn");
 const list = document.getElementById("list");
@@ -7,36 +7,60 @@ const timer = document.getElementById("timer")
 const minutes = document.getElementById("minutes")
 const seconds = document.getElementById("seconds")
 const timeButtons = document.getElementsByClassName("time-btn")
-
+const showTimeButton = document.getElementById("add-time")
+const mainHeader = document.getElementById("main-header")
 
 let pauseBool = true;
 let exArr = []
 
+
+
+/*STEP 1: Select exercises to be included in the circuit
+
+This is done by:
+a) Accessing the id of the button clicked which represents the exercise
+b) Adding the value of the button (ie the exercise) clicked to the array
+C) Option to remove the exercise from the array if clicked again
+d) Option to select all exercises
+e) Option to clear all exercises. If cleared, the time selection is hidden until exercises are selected
+f) Display the exercises selected in the list using innerHTML
+
+Please note I tried using a set rather than an array but lost the ability to remove items */
+
+
+
 const setArray=  (e)=> {
      
-  let ex = e.target.id
+  let exercise = e.target.id // Gets the id of the button clicked
 
-  if(!exArr.includes(ex)) {
-    exArr.push(document.getElementById(ex).value)
+  if(!exArr.includes(exercise)) {
+    exArr.push(document.getElementById(exercise).value) // Adds the value of the button clicked to the array
   } else{
-  let x = exArr.indexOf(ex)
-  exArr.splice(x,1)
-} if(ex === "Select All") {
+  let x = exArr.indexOf(exercise)
+  exArr.splice(x,1) // Removes the exercise from the array when clicked again
+} if(exercise === "Select All") { // Selects all exercises
   exArr = ["Burpees", "Plank", "Rest", "Sit Up", "Goblet Squat", "Kettlebell Swing", "Press Up"]  
 }
-if(ex === "Clear") {
-  exArr = []  
+if(exercise === "Clear") {
+  exArr = [] 
+  showTimeButton.style.visibility = "hidden";
+   // Clears the list of exercises 
+   // Removes the option to choose the time until exercises are selected
 }
-console.log(exArr)
 
+if (exArr.length === 0) {
+  list.innerHTML = `<div><span class="workout__value">Pick your Exercises</span></div>`;
+  showTimeButton.style.visibility = "hidden";
+  mainHeader.style.visibility = "hidden";
+} else {
   let html = `
     <div>
-      <span class="workout__value">${exArr.map(e => " " + e)}</span>
-      
+      <span class="workout__value">${exArr.map((e) => " " + e)}</span>
     </div>
-`;
-
-list.innerHTML = html;
+  `;
+  list.innerHTML = html;
+  showTimeButton.style.visibility = "visible";
+}
 }
 
 for (let button of addExerciseButtons) {
@@ -45,7 +69,22 @@ button.addEventListener("click", setArray);
 
 
 
-//Set Circuit Time
+/*STEP 2: Select the time for each circuit 
+a)  The displayTimes unction makes the option to choose a time visible only after exercises selected
+b)  The user can select the time for each circuit by inputting the minutes and seconds OR
+c) The user can select a preset time of 3, 5 or 10 minutes 
+
+
+
+*/
+// Display the time options only when exercises are selected
+const displayTimes = ()=>{
+  if (exArr.length > 0) {
+  mainHeader.style.visibility = "visible";
+} 
+}
+showTimeButton.addEventListener("click", displayTimes); //
+
 
 const setTime= ()=> { 
 
@@ -63,10 +102,7 @@ const subTime = document.getElementById("submit-time").addEventListener("click",
 
 
 
-//timeButtons - preset time button functionality 
-
-
-
+//timeButtons - preset time button functionality of 3, 5, 10 minutes
 const clickTimeButton = (e) =>{
   minutes.value = parseInt(e.target.id)
   seconds.value = 0
@@ -77,19 +113,15 @@ for (let button of timeButtons) {
 
 
 
+
+
 // Pause Functionality
 const pause = ()=> { pauseBool = !pauseBool}
 const pauseButton = document.getElementById("pause").addEventListener("click", pause)
 
 
-
-/*
-const callAllCounts = () => {
-  initialCount()
-  setTimeout(mainCount, 3000)
-  setTimeout(setExercise, 3000)
-} */
-
+ 
+// starts all countdowns
   async function callAllCounts() {
     // Start the initial countdown
     initialCount();
@@ -120,17 +152,16 @@ const initialCount = () => {
   initial.classList.remove("hide"); //Removes hidden css property when app started
   exercise.classList.remove("hide");
 
-  setInterval(function () {
+ let interval = setInterval(function () {
+  if (begin >= 1) {
+    initial.innerHTML = begin;
+    begin -= 1;
+  } else if (begin === 0) {
+    initial.innerHTML = "Go!";
+    clearInterval(interval); // Clear the interval. 
+  }
+}, 1000);
 
-    if (begin >= 1) {
-      initial.innerHTML = begin;
-      begin -= 1;
-    }
-    else if (begin === 0) {
-      initial.innerHTML = "Go!";
-    }
-
-  }, 1000); //1 second decrements
 }
 
 
